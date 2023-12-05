98b67ea24e97d2b0153abf97fbf08c1a18188826
rongliyuan <rongliyuan@gmail.com>
2023-12-05 13:41:14 +0100

    Add crowd source answers to responsor

--- chatbot/agent/responsor.py
+++ chatbot/agent/responsor.py
@@ -148,6 +148,8 @@ class Responsor:
         response_text = ""
         try:
             entity_dict, relation_dict = self.parser.parse_entity_relation(message_text)
+            print(f"entity_dict: {entity_dict}")
+            print(f"relation_dict: {relation_dict}")
         except Exception as exception:
             print(f"Identify Error: {type(exception).__name__}")
             return f"Sorry I don't understand the question: '{message_text}'. Could you please rephrase it?"
@@ -189,29 +191,52 @@ class Responsor:
                     template = random.choice(sparql_response_templates)
                     response_text = template.format(''.join(rel_lbl), ''.join(ent_lbl), answer)
                 else:
-                    print("use sparql fails, use crowdsource")
-                    use_crowdsource = True
-            if use_crowdsource:
+                    response_text = "No answer from knowledge graph. Let me check other sources... "    
+                # check crowdsource, if crowd fix exists, add it to the response
                 ent_identifier = self.emb_calculator.get_entity_identifier(ent_lbl[0])
                 rel_identifier = self.emb_calculator.get_relation_identifier(rel_lbl[0])
                 s = "wd:" + str(ent_identifier.split("/")[-1])
                 p = "wdt:" + str(rel_identifier)
                 if bool(self.crowdsource.search_crowdsource(s, p)):
-                    o,support_votes, reject_votes, kappa = self.crowdsource.search_crowdsource(s, p)
-                    if o.startswith("wd:"):
-                        o = o.replace("wd:", "")
-                        o = self.emb_calculator.ent2lbl[self.emb_calculator.WD[o]]
-                    template = random.choice(crowdsource_response_templates)
-                    response_text = template.format(o, support_votes, reject_votes, kappa)
+                    crowd_fix = self.crowdsource.search_crowdsource(s, p)
+                    if crowd_fix["fix_position"] == "Object":
+                        o = crowd_fix["fix_value"]
+                        if o.startswith("wd:"):
+                            o = o.replace("wd:", "")
+                            o = self.emb_calculator.ent2lbl[self.emb_calculator.WD[o]]
+                        support_votes = crowd_fix["support_votes"]
+                        reject_votes = crowd_fix["reject_votes"]
+                        kappa = crowd_fix["kappa"]
+                        template = random.choice(crowdsource_response_templates)
+                        response_text += "\n"
+                        response_text += template.format(o, support_votes, reject_votes, kappa)
                 else:
-                    print("use crowdsource fails, use embedding")
+                    # print("use sparql fails, use crowdsource")
+                    if not bool(sparql_result):
+                        print("No answer from sparql and crowdsource, use embedding")
+                    # use_crowdsource = True
                     use_embedding = True
+            # if use_crowdsource:
+            #     ent_identifier = self.emb_calculator.get_entity_identifier(ent_lbl[0])
+            #     rel_identifier = self.emb_calculator.get_relation_identifier(rel_lbl[0])
+            #     s = "wd:" + str(ent_identifier.split("/")[-1])
+            #     p = "wdt:" + str(rel_identifier)
+            #     if bool(self.crowdsource.search_crowdsource(s, p)):
+            #         o,support_votes, reject_votes, kappa = self.crowdsource.search_crowdsource(s, p)
+            #         if o.startswith("wd:"):
+            #             o = o.replace("wd:", "")
+            #             o = self.emb_calculator.ent2lbl[self.emb_calculator.WD[o]]
+            #         template = random.choice(crowdsource_response_templates)
+            #         response_text = template.format(o, support_votes, reject_votes, kappa)
+            #     else:
+            #         print("use crowdsource fails, use embedding")
+            #         use_embedding = True
             if use_embedding:
                 labels = ent_lbl + rel_lbl
                 try:
                     emb_results = self.emb_calculator.get_most_likely_results(labels, 10)
                 except Exception as exception:
-                    print(f"Error: {type(exception).__name__}")
+                    print(f"Embedding Error: {type(exception).__name__}")
                     return f"Sorry I don't understand the question: '{message_text}'. Could you please rephrase it?"
                 top_labels = [result['Label'] for result in emb_results[:3]]
                 template = random.choice(embedding_response_templates)
@@ -247,30 +272,31 @@ class Responsor:
 if __name__ == "__main__":
     responsor = Responsor()
     questions = [
-        "Who is the screenwriter of The Masked Gang: Cyprus?",
-        "What is the MPAA film rating of Weathering with You?",
-        "What is the country of citizenship of Olivier Schatzky?",
+        # "Who is the screenwriter of The Masked Gang: Cyprus?",
+        # "What is the MPAA film rating of Weathering with You?",
+        # "What is the country of citizenship of Olivier Schatzky?",
 
-        'When was "The Gofather" released?',
-        'Can you tell me the publication date of Tom Meets Zizou? ',#the answer is 2010-10-01 which is different from the answer from crowdsource which is 2011-01-01
+        # 'When was The Gofather released?',
+        'What is the publication date of God Help the Girl?', #the answer is 2014-01-18 which is different from the answer from crowdsource which is 2014-02-18
+        # 'Can you tell me the publication date of Tom Meets Zizou? ',#the answer is 2010-10-01 which is different from the answer from crowdsource which is 2011-01-01
 
-        "Who is the director of Star Wars: Epode VI - Return of the Jedi?",
-        "Who is the director of Good Will Huntin? ",
-        'Who directed The Bridge on the River Kwai?',
+        # "Who is the director of Star Wars: Epode VI - Return of the Jedi?",
+        # "Who is the director of Good Will Huntin? ",
+        # 'Who directed The Bridge on the River Kwai?',
 
-        "What is the genre of Good Neighbors?",
+        # "What is the genre of Good Neighbors?",
 
-        "What is the box office of The Princess and the Frog? ",
-        "Who is the executive producer of X-Men: First Class? ",
-        "What is the birthplace of Christopher Nolan? ",
+        # "What is the box office of The Princess and the Frog? ",
+        # "Who is the executive producer of X-Men: First Class? ",
+        # "What is the birthplace of Christopher Nolan? ",
 
-        "Given that I like The Lion King, Pocahontas, and The Beauty and the Beast, can you recommend some movies? ",
-        "Recommend movies like Nightmare on Elm Street, Friday the 13th, and Halloween. "
+        # "Given that I like The Lion King, Pocahontas, and The Beauty and the Beast, can you recommend some movies? ",
+        # "Recommend movies like Nightmare on Elm Street, Friday the 13th, and Halloween. "
 
-        'Show me a picture of Halle Berry. ',
-        'Show me a picture of Tom Cruise. ',
-        'What does Julia Roberts look like? ',
-        'Let me know what Sandra Bullock looks like. '
+        # 'Show me a picture of Halle Berry. ',
+        # 'Show me a picture of Tom Cruise. ',
+        # 'What does Julia Roberts look like? ',
+        # 'Let me know what Sandra Bullock looks like. '
 
     ]
     for question in questions:
--- chatbot/crowdsource/crowdsource.py
+++ chatbot/crowdsource/crowdsource.py
@@ -3,58 +3,77 @@ from statsmodels.stats.inter_rater import fleiss_kappa, aggregate_raters
 from pathlib import Path
 
 # Check if filtered data exists
-filtered_data_path = Path(__file__).parents[1].joinpath("data", "filter_crowd.csv")
-if filtered_data_path.exists():
-    print("Filtered data already exists, skipping filtering process")
-    filtered_data = pd.read_csv(filtered_data_path)
-else:
+# filtered_data_path = Path(__file__).parents[1].joinpath("data", "filter_crowd.csv")
+# if filtered_data_path.exists():
+#     print("Filtered data already exists, skipping filtering process")
+#     filtered_data = pd.read_csv(filtered_data_path)
+# else:
     # If filtered data doesn't exist, filter the raw data
-    raw_data_path = Path(__file__).parents[1].joinpath("data", "crowd_data_Corrected.tsv")
-    with open(raw_data_path, 'r', encoding='utf-8') as f:
-        raw_data = pd.read_csv(f, sep='\t')
+raw_data_path = Path(__file__).parents[1].joinpath("data", "crowd_data_Corrected.tsv")
+with open(raw_data_path, 'r', encoding='utf-8') as f:
+    raw_data = pd.read_csv(f, sep='\t')
 
-    filtered_data = raw_data[(raw_data['WorkTimeInSeconds'] > 30) & (raw_data['LifetimeApprovalRate'] > '40%')]
+filtered_data = raw_data[(raw_data['WorkTimeInSeconds'] > 30) & (raw_data['LifetimeApprovalRate'] > '40%')]
 
-    # kappas = filtered_data.groupby('HITTypeId').apply(lambda x: fleiss_kappa(aggregate_raters(pd.crosstab(x['WorkerId'], x['AnswerLabel']))[0]))
-    kappas = filtered_data.groupby('HITTypeId').apply(
-        lambda x: fleiss_kappa(pd.crosstab(x['HITId'], x['AnswerLabel']))
-    )
-    filtered_data = filtered_data.assign(
-        kappa = filtered_data['HITTypeId'].apply(lambda x: kappas[x])
-    )
+# kappas = filtered_data.groupby('HITTypeId').apply(lambda x: fleiss_kappa(aggregate_raters(pd.crosstab(x['WorkerId'], x['AnswerLabel']))[0]))
+kappas = filtered_data.groupby('HITTypeId').apply(
+    lambda x: fleiss_kappa(pd.crosstab(x['HITId'], x['AnswerLabel']))
+)
+filtered_data = filtered_data.assign(
+    kappa = filtered_data['HITTypeId'].apply(lambda x: kappas[x])
+)
 
-    data = []
-    for hitId, group in filtered_data.groupby('HITId'):
-        s = group['Input1ID'].iloc[0]
-        p = group['Input2ID'].iloc[0]
-        o = group['Input3ID'].iloc[0]
-        votes = group['AnswerLabel'].value_counts()
-        support_votes = votes.get('CORRECT', 0)
-        reject_votes = votes.get('INCORRECT', 0)
-        # kappa = kappas.get(hitId, None)
-        kappa = group['kappa'].iloc[0]
-        data.append([hitId, s, p, o, support_votes, reject_votes, kappa])
+data = []
+for hitId, group in filtered_data.groupby('HITId'):
+    s = group['Input1ID'].iloc[0]
+    p = group['Input2ID'].iloc[0]
+    o = group['Input3ID'].iloc[0]
+    votes = group['AnswerLabel'].value_counts()
+    support_votes = votes.get('CORRECT', 0)
+    reject_votes = votes.get('INCORRECT', 0)
+    fixpos = group['FixPosition'].value_counts().idxmax() if len(group['FixPosition'].value_counts()) == 1 else "NA"
+    fixval = group['FixValue'].value_counts().idxmax() if len(group['FixValue'].value_counts()) == 1 else "NA"
+    # kappa = kappas.get(hitId, None)
+    kappa = group['kappa'].iloc[0]
+    data.append([hitId, s, p, o, support_votes, reject_votes, fixpos, fixval, kappa])
 
-    result_df = pd.DataFrame(data, columns=['HITId', 'Subject', 'Predicate', 'Object', 'SupportVotes', 'RejectVotes', 'Kappa'])
-
-    result_df.to_csv('filter_crowd.csv', index=False)
+filtered_data = pd.DataFrame(data, columns=['HITId', 'Subject', 'Predicate', 'Object', 'SupportVotes', 'RejectVotes', 'FixPosition', 'FixValue','Kappa'])
+# print(filtered_data.head())
+# filtered_data.to_csv('filter_crowd.csv', index=False)
+crowd_answers = filtered_data.loc[
+    (~filtered_data["FixPosition"].isna()) &
+    (~filtered_data["FixValue"].isna())
+]
+# crowd_answers.to_csv('crowd_answers.csv', index=False)
 
 class CrowdSource:
 
     def __init__(self):
-        self.crowd_data = filtered_data
+        # self.crowd_data = filtered_data
+        self.crowd_answers = crowd_answers
 
     def search_crowdsource(self, s, p):
-        df = self.crowd_data
-        crowd_answers = df.loc[(df['Subject'] == s) & (df["Predicate"] == p)]
-        if crowd_answers.empty:
+        # df = self.crowd_data
+        # crowd_answers = df.loc[(df['Subject'] == s) & (df["Predicate"] == p)]
+        crowd_answer = self.crowd_answers.loc[
+            (self.crowd_answers['Subject'] == s) & 
+            (self.crowd_answers["Predicate"] == p)
+        ]
+        if crowd_answer.empty:
             return None
         else:
-            o = crowd_answers['Object'].iloc[0]
-            kappa = crowd_answers['Kappa'].iloc[0]
-            support_votes = crowd_answers['SupportVotes'].iloc[0]
-            reject_votes = crowd_answers['RejectVotes'].iloc[0]
-            return o,support_votes, reject_votes, round(kappa, 3)
+            crowd_fix = {
+                "subject": s,
+                "predicate": p,
+                "object": crowd_answer['Object'].iloc[0],
+                "support_votes": crowd_answer['SupportVotes'].iloc[0],
+                "reject_votes": crowd_answer['RejectVotes'].iloc[0],
+                "fix_position": crowd_answer['FixPosition'].iloc[0],
+                "fix_value": crowd_answer['FixValue'].iloc[0],
+                "kappa": round(crowd_answer['Kappa'].iloc[0], 3)
+            }
+            # return o,support_votes, reject_votes, round(kappa, 3)
+            return crowd_fix
 
 
     def merge_crowdsource(self, s, p):
@@ -62,4 +81,5 @@ class CrowdSource:
 
 if __name__ == '__main__':
     crowd = CrowdSource()
-    print(crowd.search_crowdsource('wd:Q11621', 'wdt:P2142'))
+    # print(crowd.search_crowdsource('wd:Q11621', 'wdt:P2142'))
+    print(crowd.search_crowdsource('wd:Q16911843', 'wdt:P577')) # 2014-02-18
