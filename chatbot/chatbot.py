a43f867da3f8aaf90015dd585a238e1cdb928d22
hangchenxie <114024510+hangchenxie@users.noreply.github.com>
2024-07-27 11:38:47 +0200

    delete credentials

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -3,7 +3,7 @@ from chatbot.agent.responsor import Responsor
 
 
 def DemoBot():
-    demo_bot = Agent("torch-staccato-mushroom_bot", "ofmkiY2qPQeiRg")
+    demo_bot = Agent("NAME OF bot", "PASSWORD")
     demo_bot.listen()
 
 
